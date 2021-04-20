772dcb1f320cf66b0eb4d50e20aa1343764a6cf1
Bs1987 <boysax@gmail.com>
2021-04-20 21:05:36 +0200

    First steps in hangman, made a loop

--- gamelogic.js
+++ gamelogic.js
@@ -1,17 +1,46 @@
+// Input:
+// Word: "javascript"
+// Guesses: []
+//
+// Output:
+// _ _ _ _ _ _ _ _ _ _
+
+// javascript => _ _ _ _ _ _ _ _ _ _
+
+// Steps:
+//  - transformeren
+//  - for loop maken:
+//    - elke letter vervangen door een "_ "
+//    - Tot we alle letters gehad hebben van het woord
+//  - return resultaat
+//  -
+//  - functies
+
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+    let output = "";
+    for (let i = 0; i < 10; i++) {
+        output = output + "_ ";
+        console.log("WORD[I] IS: ", word[i]);
+        console.log("OUTPUT IS: ", output);
+        return output;
+    }
+console.log("What is the output", displayWordSoFar("javascript", []));
+
+
 }
 
+displayWordSoFar("javascript", []);
+
 function isGameWon(word, guesses) {
-  // WRITE ME
+    // WRITE ME
 }
 
 function isGameLost(word, guesses) {
-  // WRITE ME
+    // WRITE ME
 }
 
 module.exports = {
-  displayWordSoFar: displayWordSoFar,
-  isGameWon: isGameWon,
-  isGameLost: isGameLost,
+    displayWordSoFar: displayWordSoFar,
+    isGameWon: isGameWon,
+    isGameLost: isGameLost,
 };
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+
+// we importeren readline, we halen de question function eruit
 const { question } = require("readline-sync");
+
+// we destructureren variabelen uit gamelogic
 const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
 
+// we definieren een functie game
+// parameter 1: word
+// parameter 2: guesses
 function game(word, guesses) {
+  // we loggen de guesses parameter (feedback user)
   console.log("Dit heb je tot nu toe geraden: ", guesses);
 
+  // we roepen de question function aan, de return waarde slaan we op in de letter variabele
   const letter = question("Raad een letter: ");
 
   // voeg de geraden letter toe aan de array met guesses
@@ -13,6 +22,7 @@ function game(word, guesses) {
   game(word, guesses);
 }
 
+// we loggen een glagje logo met een template string
 console.log(`
 __________  
 | /     |    ░██████╗░░█████╗░██╗░░░░░░██████╗░░░░░░██╗███████╗
@@ -23,4 +33,7 @@ __________
 ===========  ░╚═════╝░╚═╝░░╚═╝╚══════╝░╚═════╝░░╚════╝░╚══════╝
 `);
 
+//de game functie wordt aangeroepen met:
+// argument 1: string
+// argument 2: lege array
 game("javascript", []);
